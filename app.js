1eb914203ce7aee6c1bc726588c19d3cdb20d51b
Siddharth Choudhari <choudharishiddharth@gmail.com>
2023-01-03 17:43:27 +0530

    fix port

--- app.js
+++ app.js
@@ -7,19 +7,19 @@ const app = express();
 app.use(express.static('public'))
 app.set("view engine", "ejs");
 
-const port = 3000;
-
-app.get("/", (req, res)=> {
+app.get("/", (req, res) => {
     const URL = "https://shibe.online/api/shibes?count=1";
-    https.get(URL, (response)=> {
-        response.on("data", (image)=> {
+    https.get(URL, (response) => {
+        response.on("data", (image) => {
             const shibeImage = JSON.parse(image)
             const shibeURL = shibeImage[0]
-            res.render("index", {randomShiba: shibeURL})
+            res.render("index", { randomShiba: shibeURL })
         })
     })
 })
 
-app.listen(process.env.PORT || port, ()=>{
-    console.log("Server is up and running at port 3000");
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log(`Server is up and running at port ${PORT}`);
 })
